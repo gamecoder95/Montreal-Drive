777e756fa6d589d992e30bc59a3fa3426a6114da
Ahmad El-Baher <ahmad.elbaher@gmail.com>
2017-01-01 18:55:48 -0500

    Started Stage 1 of development
    On the branch stage 1, this will be the branch where stage 1 of the
    development will take place. Stage 1 involves creating the basic
    gameplay including: basic test movement of player, basic obstacles,
    and a simulation of endless scolling using a background.
    
    This commit is the beginning of this stage of development.

---
+++ Montreal_Drive/Assets/Scripts/Car.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Car : MonoBehaviour {
+
+	public float moveSpeedX;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		moveCar ();
+	}
+
+	void moveCar()
+	{
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			this.transform.position += new Vector3 (-moveSpeedX, 0, 0);
+		} else if (Input.GetKey (KeyCode.RightArrow)) {
+			this.transform.position += new Vector3 (moveSpeedX, 0, 0);
+		} 
+	}
+
+
+}
---
+++ Montreal_Drive/Assets/Scripts/Obstacle.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Obstacle : MonoBehaviour {
+
+	public float moveSpeedY;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		scrollDown ();
+	
+	}
+
+	void scrollDown ()
+	{
+		if (this.transform.position.y >= 0) {
+			this.transform.position += new Vector3 (0, -moveSpeedY, 0);
+		} else {
+			Destroy (gameObject);
+		}
+	}
+
+
+}
