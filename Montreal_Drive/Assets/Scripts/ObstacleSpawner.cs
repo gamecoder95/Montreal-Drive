579b7b1eae2e2989cf146b51a70af9068ad1bafc
Ahmad El-Baher <ahmad.elbaher@gmail.com>
2017-01-05 22:33:07 -0500

    Began making obstacle spawner
    Using Space bar to test it out, got it to spawn above
    visible window at random x-coordinates and spaced out
    so they don't overlap.
    
    TODO: make spacing random, and make obstacle spawning
    automated.

---
+++ Montreal_Drive/Assets/Scripts/ObstacleSpawner.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObstacleSpawner : MonoBehaviour {
+
+	public ScrollingObject obstacle;
+	public float lowerLimitSpawn_x;
+	public float upperLimitSpawn_x;
+	private float spawnPoint_y;
+	private float obstacleHeight;
+	private float cameraTop_y;
+	private const float EXTRA_HEIGHT = 2.0f;
+
+	// Use this for initialization
+	void Start () {
+		obstacleHeight = obstacle.GetComponent<SpriteRenderer> ().bounds.size.y;
+		cameraTop_y = Camera.main.orthographicSize;
+		spawnPoint_y = cameraTop_y + obstacleHeight + EXTRA_HEIGHT;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// Temporary; the aim is to spawn obstacles periodically, using a defined time interval.
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			this.spawnObstacleAtRandomPoint ();
+		}
+	}
+
+	void spawnObstacle(float x, float y)
+	{
+		Instantiate (obstacle, new Vector3 (x, y, 0.0f), Quaternion.identity);
+	}
+
+	void spawnObstacleAtRandomPoint()
+	{
+		float spawnPoint_x = Random.Range (lowerLimitSpawn_x, upperLimitSpawn_x);
+		if (isLatestObstacleAboveScreen ()) {
+
+			GameObject latestSpawnedObstacle = getLatestSpawnedObstacle ();
+			float latestSpawnedObstacle_y = latestSpawnedObstacle.transform.position.y;
+			float spawnPointAboveLatestObstacle_y = latestSpawnedObstacle_y + obstacleHeight + EXTRA_HEIGHT;
+
+			spawnObstacle (spawnPoint_x, spawnPointAboveLatestObstacle_y);
+			
+		} else {
+			spawnObstacle (spawnPoint_x, spawnPoint_y);
+		}
+	}
+
+	GameObject getLatestSpawnedObstacle()
+	{
+		GameObject[] spawnedObstacles = GameObject.FindGameObjectsWithTag ("Obstacle");
+		if (spawnedObstacles.Length == 0) {
+			return null;
+		} else {
+			return spawnedObstacles [spawnedObstacles.Length - 1];
+		}
+	}
+
+	bool isLatestObstacleAboveScreen()
+	{
+		GameObject latestSpawnedObstacle = getLatestSpawnedObstacle ();
+
+		if (latestSpawnedObstacle == null) {
+			return false;
+		} else {
+			
+			float latestSpawnedObstacle_y = latestSpawnedObstacle.transform.position.y;
+			float latestSpawnedObstacleBottom_y = latestSpawnedObstacle_y + obstacleHeight;
+
+			return latestSpawnedObstacleBottom_y >= cameraTop_y;
+		}
+			
+
+	}
+		
+}
